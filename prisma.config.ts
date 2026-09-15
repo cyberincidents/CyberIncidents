6f35ea06d62196da6c196928797662a0fe592244
Akshay-2024 <akshay614325@gmail.com>
2026-09-15 11:57:09 +0530

    prisma update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
+  },
+
+  datasource: {
+    url: env("DATABASE_URL"),
   },
 });
